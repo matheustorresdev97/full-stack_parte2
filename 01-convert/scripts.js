4aa34cc662827d29ae50785941e58966c438ef6e
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-26 20:37:23 -0300

    exibindo o footer e a cotação da moeda

--- 01-convert/scripts.js
+++ 01-convert/scripts.js
@@ -7,6 +7,8 @@ const GBP = 7.74;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -33,5 +35,15 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol);
+  try {
+    description.textContent = `${symbol} 1 = ${price}`;
+
+    footer.classList.add("show-result");
+  } catch {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result");
+
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde.");
+  }
 }
