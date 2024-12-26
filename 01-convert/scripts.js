6b2c1919f88959762d75d1c7baa77897f0a55ce2
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-26 20:32:43 -0300

    obtendo o valor digitado

---
+++ 01-convert/scripts.js
@@ -0,0 +1,5 @@
+const amount = document.getElementById("amount");
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
+  console.log(amount.value);
+});
