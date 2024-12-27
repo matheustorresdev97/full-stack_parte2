6cc571da7c558ebe28335c7533f34d3f6727afd6
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-26 21:57:12 -0300

    criando o icone da categoria

--- 03-refund/scripts.js
+++ 03-refund/scripts.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
+// Seleciona os elementos da lista
+const expenseList = document.querySelector("ul");
+
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
   // Obtém o valor atual do input e remove os caracteres não numéricos
@@ -45,6 +48,18 @@ form.onsubmit = (event) => {
 
 function expenseAdd(newExpense) {
   try {
+    // Cria o elemento para a adicionar o item (li) na lista (ul)
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // Cria o ícone da categoria
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon);
+    // Adiciona o item na lista
+    expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
